84c19beefe7d5cfe5f85875bd69ff7afca9334a1
valdery <valdery.jur@gmail.com>
2019-12-11 10:24:36 -0300

    :construction: Estrutura a página home
    Signed-off-by: valdery <valdery.jur@gmail.com>

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { StyledSectionCard } from '../../components/section-card';
+import { MusicCardComponent } from '../../components/music-card';
+import { SearchFormComponent } from '../../components/search-form';
 
 export default () => {
     return (
-        <p>Home page</p>
+        <>
+            <StyledSectionCard>
+                <h2>FILTRO DOS ARTISTAS</h2>
+            </StyledSectionCard>
+            <StyledSectionCard>
+                <h2>PAINEL DE ÁLBUNS</h2>
+            </StyledSectionCard>
+            <StyledSectionCard>
+                <h2>TODAS AS MÚSICAS DO ARTISTA</h2>
+                <SearchFormComponent />
+                <MusicCardComponent
+                    albumNumber={1} 
+                    trackNumber={1}
+                    musicName={'Starway to Heaven'}
+                    popularity={'Alta'}
+                    explicit={true} />
+            </StyledSectionCard>
+        </>
     )
 }
