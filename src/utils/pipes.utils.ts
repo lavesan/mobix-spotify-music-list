62a1e78e3474d0e34d4c5861a1a99d5c75bb5e6b
valdery <valdery.jur@gmail.com>
2019-12-12 01:19:00 -0300

    :memo: Tipa o retorna da função
    Signed-off-by: valdery <valdery.jur@gmail.com>

--- src/utils/pipes.utils.ts
+++ src/utils/pipes.utils.ts
@@ -1,8 +1,8 @@
 /**
  * @description Add a '0' in front of 1 digit numbers
- * @param number 
+ * @param {number | string} number 
  */
-export const addZeroOnNumber = function(number: number | string = 0) {
+export const addZeroOnNumber = function(number: number | string = 0): string {
     const numberString = number.toString();
 
     return numberString.length === 1 ?
