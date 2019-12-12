6926e5d4687678ea7df0bb22fce53b50651c3d2b
valdery <valdery.jur@gmail.com>
2019-12-12 01:20:38 -0300

    :nail_care: Adiciona display grid contendo a estilização para a primeira section da página
    Signed-off-by: valdery <valdery.jur@gmail.com>

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 import { StyledSectionCard } from '../../components/section-card';
 import { MusicCardComponent } from '../../components/music-card';
 import { SearchFormComponent } from '../../components/search-form';
-import { StyledMusicCardsBox, StyledH2 } from './home.styles';
+import { StyledMusicCardsBox, StyledH2, StyledArtistsFilterBox } from './home.styles';
+import { SelectComponent } from '../../components/select';
+import { RangePickerComponent } from '../../components/range-picker';
 
 export default () => {
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+    ]
+
     return (
         <>
             <StyledSectionCard>
                 <StyledH2>FILTRO DOS ARTISTAS</StyledH2>
+                <StyledArtistsFilterBox>
+                    <SelectComponent
+                        options={options}
+                        placeholder="Artistas" />
+                    <RangePickerComponent
+                        label="Periodo" />
+                </StyledArtistsFilterBox>
             </StyledSectionCard>
             <StyledSectionCard>
                 <StyledH2>PAINEL DE ÁLBUNS</StyledH2>
--- src/pages/home/home.styles.ts
+++ src/pages/home/home.styles.ts
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
+export const StyledArtistsFilterBox = styled.section`
+    display: grid;
+    grid-template-columns: 3fr 3fr 1fr ;
+    grid-column-gap: 30px;
+    width: 50%;
+    margin-bottom: 30px;
+`;
+
 export const StyledMusicCardsBox = styled.section`
     display: grid;
     grid-template-columns: 3fr 3fr 3fr 3fr;    
