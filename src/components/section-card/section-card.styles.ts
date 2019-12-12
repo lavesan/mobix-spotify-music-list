dcf561cbedd805e89b881c39ffdc03a312d15554
valdery <valdery.jur@gmail.com>
2019-12-11 23:50:09 -0300

    :nail_care: Altera a largura do card de seção
    De 90% para 80%
    
    Signed-off-by: valdery <valdery.jur@gmail.com>

--- src/components/section-card/section-card.styles.ts
+++ src/components/section-card/section-card.styles.ts
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const StyledSectionCard = styled.section`
     box-shadow: 0 0 6px #c2c2c2;
     border-radius: 5px;
-    padding: 10px;
+    padding: 10px 50px;
     background-color: #fff;
-    /* width: fit-content; */
-    width: 90%;
+    width: 80%;
 `;
