516fbd4f7a85764abf5fefb4c76cce7e1077a580
valdery <valdery.jur@gmail.com>
2019-12-14 03:56:39 -0300

    :nail_care: Adiciona responsividade nos cards de música em todos tamanhos de tela
    Foram utilizadas as media queries
    
    Signed-off-by: valdery <valdery.jur@gmail.com>

--- src/pages/home/section3/section3.styles.ts
+++ src/pages/home/section3/section3.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledMusicCardsBox = styled.section`
     display: grid;
-    grid-template-columns: 3fr 3fr 3fr 3fr;    
+    grid-template-columns: 1fr 1fr 1fr 1fr;
     grid-gap: 30px;
     min-height: 0;
     min-width: 0;
@@ -10,4 +10,16 @@ export const StyledMusicCardsBox = styled.section`
     margin-top: 50px;
     max-height: 350px;
     overflow-y: scroll;
+
+    @media(max-width: 1100px) {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+
+    @media(max-width: 860px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media(max-width: 600px) {
+        grid-template-columns: 1fr;
+    }
 `;
