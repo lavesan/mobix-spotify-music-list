14206b8fca819d9660251a4eda6821125e48f7eb
valdery <valdery.jur@gmail.com>
2019-12-13 21:52:46 -0300

    :construction: Adiciona botão para se logar no spotify e obter o token de autenticação
    Signed-off-by: valdery <valdery.jur@gmail.com>

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyledSectionCard } from '../../components/section-card';
 import { MusicCardComponent } from '../../components/music-card';
 import { SearchFormComponent } from '../../components/search-form';
-import { StyledMusicCardsBox, StyledH2, StyledArtistsFilterBox } from './home.styles';
+import { StyledMusicCardsBox, StyledH2, StyledArtistsFilterBox, StyledLogSpotifyLink } from './home.styles';
 import { SelectComponent } from '../../components/select';
 import { RangePickerComponent } from '../../components/range-picker';
+import { BarChartComponent } from '../../components/bar-chart';
+import { SpotifyService } from '../../services/spotify';
+import { queryStringToObject } from '../../utils/app.utils';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpotify } from '@fortawesome/free-brands-svg-icons';
 
 export default () => {
+    const spotifyService = new SpotifyService();
     const options = [
-        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'michael jackson', label: 'Chocolate' },
         { value: 'strawberry', label: 'Strawberry' },
         { value: 'vanilla', label: 'Vanilla' }
     ]
+
+    useEffect(() => {
+        const authToken = queryStringToObject(window.location.href);
+        if (authToken) {
+            spotifyService.saveAuthOnCookie(authToken);
+        }
+        // const accessToken = queryString.parse(window.location.access_token)
+        setTimeout(() => {
+            spotifyService.getArtistAlbum('Michael jackson');
+        }, 4000);
+    }, []);
 
     return (
         <>
@@ -24,10 +41,14 @@ export default () => {
                     <RangePickerComponent
                         label="Periodo"
                         initialValue={2019} />
+                    <StyledLogSpotifyLink href="https://accounts.spotify.com/pt-BR/authorize?client_id=f27147875f9843e9898af17332049543&response_type=token&redirect_uri=http:%2F%2Flocalhost:3000%2Fhome">
+                        <FontAwesomeIcon icon={faSpotify} />&nbsp;&nbsp;Logar-se no spotify
+                    </StyledLogSpotifyLink>
                 </StyledArtistsFilterBox>
             </StyledSectionCard>
             <StyledSectionCard>
                 <StyledH2>PAINEL DE ÁLBUNS</StyledH2>
+                <BarChartComponent />
             </StyledSectionCard>
             <StyledSectionCard>
                 <StyledH2>TODAS AS MÚSICAS DO ARTISTA</StyledH2>
